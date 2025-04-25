d94d582ab0752aa23993b7f97b67555f3b2c29e8
Michael Lang <115140776+hda-typo3@users.noreply.github.com>
2025-04-25 13:36:29 +0200

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'h_da - Installation',
     'description' => '',
     'category' => 'templates',
-    'version' => '12.0.01',
+    'version' => '13.0.01',
     'state' => 'alpha',
     'clearcacheonload' => true,
     'author' => 'Michael Lang',
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '12.4 -',
+            'typo3' => '13.4 -',
         ),
         'conflicts' => array(
             'fluidpages' => '*',
